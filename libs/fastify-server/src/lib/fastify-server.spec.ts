5f48c50d222219dd99da9498f6c627f5f89d5f40
NaderBhrr <nader.bhr.devv@gmail.com>
2022-04-05 12:05:50 +0430

    ✨ Create the fastify-server library for all services

--- libs/fastify-server/src/lib/fastify-server.spec.ts
+++ libs/fastify-server/src/lib/fastify-server.spec.ts
@@ -2,6 +2,7 @@ import { fastifyServer } from './fastify-server';
 
 describe('fastifyServer', () => {
   it('should work', () => {
+    console.log("reachin here")
     expect(fastifyServer()).toEqual('fastify-server');
   });
 });
--- libs/fastify-server/src/lib/fastify-server.ts
+++ libs/fastify-server/src/lib/fastify-server.ts
@@ -1,3 +1,15 @@
-export function fastifyServer(): string {
-  return 'fastify-server';
+import Fastify, { FastifyInstance, FastifyPluginAsync } from "fastify";
+import fastifyPlugin from "fastify-plugin";
+
+export const fastifyServer = async function (...plugins: FastifyPluginAsync[]): Promise<FastifyInstance> {
+
+  const fastifyInstance: FastifyInstance = Fastify({});
+
+  fastifyInstance.register(import("fastify-cors"));
+
+for await (const plugin of plugins) {
+  fastifyInstance.register(fastifyPlugin(plugin));
+}
+
+  return fastifyInstance;
 }
