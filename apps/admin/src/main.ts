49c05f185bf23cd692d4590060d172221dcc5da9
NaderBhrr <nader.bhr.devv@gmail.com>
2022-04-05 12:38:03 +0430

    ✨ Create the admin server in 'admin' service

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import { fastifyServer, displayServerRunning } from "@bookmind/fastify-server";
+
+const adminServiceServer = async function () {
+
+    const server = fastifyServer();
+
+    try {
+        
+        const port = process.env.PORT || 4000;
+
+        (await server).ready();
+        (await server).listen(port, '0.0.0.0', () => {
+            displayServerRunning("ADMIN", port);
+        });
+
+    } catch (error) {
+        (await server).log.error(error);
+        process.exit(1);
+    }
+}
+
+adminServiceServer();
