b06f7c97159ac011fb20c68266209ab4ec77e508
NaderBhrr <nader.bhr.devv@gmail.com>
2022-04-07 05:14:39 +0430

    ✨ Create the tribe facade library to consume by services

--- libs/tribe-facade/src/lib/tribe-facade.ts
+++ libs/tribe-facade/src/lib/tribe-facade.ts
@@ -1,3 +1,27 @@
-export function tribeFacade(): string {
-  return 'tribe-facade';
+import {TribeClient} from "@tribeplatform/gql-client";
+
+export function tribeFacade() {
+
+  const client = new TribeClient({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    graphqlUrl: process.env.GRAPHQL_URL
+  });
+
+  async function getTribeAccessToekn (client: TribeClient) {
+
+    const token = await client.generateToken({
+      networkId: process.env.NETWORK_ID,
+      memberId: process.env.MEMBER_ID
+    });
+
+    console.log("Toekn >>", token);
+
+    return token
+
+  }
+  return {
+    client,
+    getTribeAccessToekn
+  }
 }
